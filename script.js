59a6c7d9ead6617b05bc3b6c9887a2d0f4e96150
Hitesh Yadav <hiteshy468@gmail.com>
2023-10-10 12:05:17 +0530

    cursor update

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const toggle = document.querySelector(".toggle")
 const nav = document.querySelector(".navigation")
 
 toggle.addEventListener('click',()=>{
-    // console.log('hello')
     nav.classList.toggle('mobile')
 })
+
+let cur = document.querySelector('.cur')
+
+document.addEventListener('mousemove' , function jkl(e){
+    cur.style.left= e.pageX + 'px';
+    cur.style.top= e.pageY + 'px';
+})
+
